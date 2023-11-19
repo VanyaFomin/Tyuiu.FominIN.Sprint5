bbbcca4a037e913f10cde9026878a00346702e05
Tyuiu.FominVN@outlook.com <hialdez111@gmail.com>
2023-11-20 00:37:41 +0500

    Task3

---
+++ Tyuiu.FominIN.Sprint5.Task3.V5.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.FominIN.Sprint5.Task3.V5.Lib
+{
+    public class DataService : ISprint5Task3V5
+    {
+        public string SaveToFileTextData(int x)
+        {
+            double result = 2 * Math.Pow(x, 3) + 0.5 * Math.Pow(x, 2) - 3.5 * x + 2;
+
+            result = Math.Round(result, 3);
+
+            string outputFile = "OutPutFileTask3.bin";
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(outputFile, FileMode.Create)))
+            {
+                writer.Write(result);
+            }
+
+            Console.WriteLine($"Результат выражения F({x}) = {result}");
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Tyuiu.FominIN.Sprint5.Task3.V5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FominIN.Sprint5.Task3.V5.Lib;
+namespace Tyuiu.FominIN.Sprint5.Task3.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Фомин И. Н. | ПКТб-23-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Тема: Обработка файлов                                                       *");
+            Console.WriteLine("* Задание #3                                                                   *");
+            Console.WriteLine("* Вариант #5                                                                   *");
+            Console.WriteLine("* Выполнил: Фомин Иван Николаевич | ПКТб-23-1                                  *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Дано выражение F(x) = 2x^3 + 1/2x^2 - 3.5x + 2,                              *");
+            Console.WriteLine("* вычислить его значение при x = 2                                             *");
+            Console.WriteLine("* результат сохранить в бинарный файл OutPutFileTask3.bin                      *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine("x = " + x);
+ 
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " );
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+
+        }
+    }
+}
