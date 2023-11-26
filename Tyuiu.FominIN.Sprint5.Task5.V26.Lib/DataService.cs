a6ad794e9c497af8ee8c26d9d9e587d509d5d3ac
Tyuiu.FominVN@outlook.com <hialdez111@gmail.com>
2023-11-26 14:09:19 +0500

    Task4

---
+++ Tyuiu.FominIN.Sprint5.Task5.V26.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.FominIN.Sprint5.Task5.V26.Lib
+{
+    public class DataService : ISprint5Task5V26
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double x = Convert.ToDouble(strX);
+            double res = (Math.Pow(x, 3) + Math.Sin(x)) * Math.Cos(x);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.FominIN.Sprint5.Task5.V26.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+using Tyuiu.FominIN.Sprint5.Task5.V26.Lib;
+namespace Tyuiu.FominIN.Sprint5.Task5.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedLoad()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V26.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
